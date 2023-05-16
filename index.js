a63177f33a07d0a8d2dab31ae62dbae5e7fce483
Denilsonaraujoprogramador <101232538+Denilsonaraujoprogramador@users.noreply.github.com>
2023-05-16 19:03:12 -0300

    Ajustando a função

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 
 var area = document.getElementById("acessar");
-// Função BEM VINDO------------------------------------------------------------------------------------------
+
+var nomeCadastro = "Denilson Araujo";
+
 function entrar() {
     var nome = prompt("Digite seu nome");
+    if(nome == nomeCadastro){
+        area.innerHTML = "Bem Vindo " + nome + "<br>";
+        let botao = document.getElementById('botaoAcessar');
+        botao.style.display = "none";
 
-    if(nome=="" || nome==null){
-        alert("Por favor preencher todos os campos");
-        area.innerHTML = "Click no botão para acessar: "
+        let botaoSair = document.createElement("button");
+        botaoSair.innerText = "Sair da conta";
+        botaoSair.onclick = sair;
+        area.appendChild(botaoSair);
+
     }else {
-        area.innerHTML = "Bem Vindo " + nome + "<br>"
+        alert("Nome não localizado no cadastro, por favor realizar cadastro");
+        area.innerHTML = "Click no botão para acessar novamente: ";
     }
 }
+
+function sair(){
+    alert("Até mais!");
+    area.innerHTML = "Você Saiu!";
+    let botao = document.getElementById('botaoAcessar');
+    botao.style.display = "";
+}
+
+
+
+// Script do professor___________________________________________________________________________________
+
+// let area = document.getElementById('area');
+// let botao = document.getElementById("botao");
+// nomeCadastro = "Antonio César";
+// function entrar() {
+//     let nome = prompt("Digite o seu nome");
+
+//     if (nome === '' || nome === null) {
+//         alert("Ops, algo deu errado!!!");
+//         area.innerHTML = "Clique no botão para acessar";
+//     } else {
+//         if (nomeCadastro == nome) {
+//             area.innerHTML = "Bem vindo " + nome + "<br>";
+//             let botaoSair = document.createElement("button");
+//             botaoSair.innerText = "Sair da conta";
+//             botaoSair.onclick = sair;
+//             area.appendChild(botaoSair);
+//             botao.setAttribute("hidden","true");
+//         }else{
+//             alert("Nome não cadatrado, favor realizar cadastro!")
+//         }
+//     }
+// }
+
+// function sair() {
+//     alert("Até mais!");
+//     area.innerHTML = "Você saiu"
+//     botao.removeAttribute("hidden","false");
+// }
+
